c6fc9ff0a8d92679a1d133b6e3f2a701a562c478
falkolab <falko.lab@gmail.com>
2014-08-14 19:26:17 +0400

    initial

---
+++ app/controllers/index.js
@@ -0,0 +1,13 @@
+function doClick1(e) {
+    $.resetClass($.premiumIcon, 'premium1');
+}
+
+function doClick2(e) {
+    $.resetClass($.premiumIcon, 'premium2');
+}
+
+function doClick3(e) {
+    $.resetClass($.premiumIcon, 'premium3');
+}
+
+$.index.open();
